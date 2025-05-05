79938b348764345d43b884116620124ad6b112e3
Tyler MacPherson <tyler.macpherson@alertinnovation.com>
2025-05-05 13:52:49 -0400

    fixing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Microservice Frontend heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Microservice Frontend/i);
+  expect(heading).toBeInTheDocument();
 });
