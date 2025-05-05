5c5483a6935369da7188ac75a83fb55dd11fadde
Tyler MacPherson <tyler.macpherson@alertinnovation.com>
2025-05-05 14:48:22 -0400

    getting db tests working

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,13 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    db_url = os.getenv("DATABASE_URL")
+    db_user = os.getenv("DB_USER", "postgres")
+    db_pass = os.getenv("DB_PASSWORD", "postgres")
+    db_host = os.getenv("DB_HOST", "localhost")
+    db_name = os.getenv("DB_NAME", "postgres")
+    db_port = os.getenv("DB_PORT", "5432")
+
+    db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
     app.db_engine = create_engine(db_url, future=True)
 
     app.register_blueprint(routes_bp)
